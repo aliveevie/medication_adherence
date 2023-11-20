843ed2e900d9b83e6ecf5f00814e0f838637aa4d
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-20 15:25:50 +0100

    Medication Adherence setup

--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@ const port = 3000
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
 });
 
 app.listen(port, () => {

8a14b2deb50181ddadd95daaebc43b4d059d7bbe
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-21 20:46:20 +0100

    Update many files

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Patient from './components/patient';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        
-      });
-  }, []);
-
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Medical Adherence App</h1>
-        <h3>We are working on it, please stay tuned!</h3>
-        <h3>Group Members:</h3>
-        <ul>
-          {data.map((member) => (
-            <li key={member.personid}>
-              <strong>{member.fullname}</strong>
-              <p>Email: {member.emailaddress}</p>
-              <p>Phone Number: {member.phonenumber}</p>
-            </li>
-          ))}
-        </ul>
+       <Patient />
       </header>
     </div>
   );

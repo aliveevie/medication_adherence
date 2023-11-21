86f269dcf564a7068417d853351a545d192caea7
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-21 11:52:20 +0100

    Update: files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState([]);
@@ -9,41 +8,30 @@ function App() {
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
   }, []);
 
-
   return (
-   <div className="App">
+    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {data.length === 0 ? (
-          <p>Loading...</p>
-        ) : (
-          <div>
-            <h2>School Data</h2>
-            <ul>
-              {data.map(school => (
-                <li key={school.school_id} className="school-item" >
-                  <strong>School ID:</strong> {school.school_id}<br />
-                  <strong>Tokens:</strong> {school.tokens}<br />
-                  <strong>School Name:</strong> {school.school_name}<br />
-                  <strong>Administrator:</strong> {school.administrator}<br />
-                  <strong>Contact Name:</strong> {school.contact_name}<br />
-                  <strong>Phone Number:</strong> {school.phone_number}<br />
-                  <strong>Email Address:</strong> {school.email_address}<br />
-                  <strong>School Address:</strong> {school.school_address}<br />
-                  <strong>Password:</strong> {school.password}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+        <h1>Medical Adherence App</h1>
+        <h3>We are working on it, please stay tuned!</h3>
+        <h3>Group Members:</h3>
+        <ul>
+          {data.map((member) => (
+            <li key={member.personid}>
+              <strong>{member.fullname}</strong>
+              <p>Email: {member.emailaddress}</p>
+              <p>Phone Number: {member.phonenumber}</p>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
-  
   );
-
 }
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -9,16 +9,17 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 
-app.get('*', (req, res) => {
+app.get('/api', async (req, res) => {
+    const result = await db.query('SELECT * FROM people');
+    console.log(result.rows)
+    res.json(result.rows);
+});
+
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 
-
-app.get('/api', async (req, res) => {
-    const result = await db.query('SELECT * FROM schools_info');
-    console.log(result.rows);
-    res.json(result.rows);
-});
 
 // const result = await db.query('SELECT school_id FROM schools_info WHERE email_address=$1', [email]);
 

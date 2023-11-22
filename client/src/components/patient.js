6f1649fb8cf45ad9c495b850f6f598768d0f0d04
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-22 13:58:30 +0100

    Feat: Update many files

--- client/src/components/patient.js
+++ client/src/components/patient.js
@@ -4,19 +4,30 @@ import logo from '../icons/logo2.svg'
 import PatientSignUp from '../pages/patientReg';
 
 export default function Patient() {
+
+  const [showPatient, setShowPatient] = useState(false);
+  const handlePatientRegister = () => {
+    setShowPatient(true)
+  }
+  
  
   return (
-    <div className='patient' >
+    !showPatient ? (
+      <div className='patient' >
       <img src={logo} alt="Logo" />
       <div className='button-class' >
-        <button className='buttons' >
+        <button className='buttons' 
+        onClick={handlePatientRegister}
+        >
            I'm a patient 
         </button>
         <button className='buttons' >
            I'm a Doctor 
         </button>
       </div>
-      <PatientSignUp />
+     
     </div>
+    ) :  <PatientSignUp />
+    
   );
 }

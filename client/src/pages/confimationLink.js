024ee257d43a19ae9e030b71f39e7b699eae7eb9
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-22 14:48:15 +0100

    Feat: Many files

---
+++ client/src/pages/confimationLink.js
@@ -0,0 +1,25 @@
+// SignupForm.js
+import React, { useState } from 'react';
+import '../styles/AuthForm.css';
+import logo from '../icons/logo.svg';
+import link from '../icons/link.svg';
+
+const Confirmation = () => {
+  
+  return (
+    <form className="auth-form" onSubmit={handleSubmit} method="post" action="/">
+      <img src={logo} alt="logo" />
+      <div className='form-header' >
+          <h2>Let’s verify your email</h2>
+        <p>
+            A verification link was sent to your email
+        </p>
+      </div>
+      <img 
+      src={link} alt='Verfication Link Icon'
+      />
+    </form>
+  );
+};
+
+export default Patient;

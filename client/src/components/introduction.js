c5b7dd0ae5022e0693a50e1aaaef3b31d77e6a1f
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-22 11:16:35 +0100

    Feat: many files

--- client/src/components/introduction.js
+++ client/src/components/introduction.js
@@ -1,28 +1,33 @@
 import '../styles/introduction.css';
-import { Link } from 'react-router-dom';
-import LoginForm from '../pages/login';  // Assuming this should be imported from login instead of signup
-import SignupForm from '../pages/signup';  // Assuming this should be imported from signup instead of login
+import Patient from './patient';
+import { useState } from 'react';
 
 export default function Introduction() {
-    return (
-        <div className="introduction">
-            <div className="text">
-                <p className='text-header'>
-                    Take control of your health with
-                </p>
-                <h2>Med<span className="text-color">Ease</span> </h2>
-                <p>Your Personal Health Companion!</p>
-                <p>
-                    Seamlessly manage your medications
-                </p>
-                <a className="buttons" href="../pages/login">
-                    Discover MedEase Now
-                </a>
+  const [showPatient, setShowPatient] = useState(false);
 
-                <a className="buttons" to="../pages/signup">
-                    Discover MedEase Doctor
-                </a>
-            </div>
+  function handleShowPatient() {
+    setShowPatient(true);
+  }
+
+  return (
+    !showPatient ? (
+      <div className="introduction">
+        <div className="text">
+          <p className='text-header'>
+            Take control of your health with
+          </p>
+          <h2>Med<span className="text-color">Ease</span> </h2>
+          <p>Your Personal Health Companion!</p>
+          <p>
+            Seamlessly manage your medications
+          </p>
+          <p className="buttons" onClick={handleShowPatient}>
+            Discover MedEase Now
+          </p>
         </div>
-    );
+      </div>
+    ) : (
+      <Patient />
+    )
+  );
 }
--- client/src/components/patient.js
+++ client/src/components/patient.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import '../styles/patient.css'
+import logo from '../icons/logo2.svg'
 
 export default function Patient() {
  
   return (
     <div className='patient' >
-      <img src='../icons/logo.svg' />
+      <img src={logo} alt="Logo" />
+      <div className='button-class' >
+        <button className='buttons' >
+           I'm a patient 
+        </button>
+        <button className='buttons' >
+           I'm a Doctor 
+        </button>
+      </div>
     </div>
   );
 }

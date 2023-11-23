b7f53041f2c8854bfb1c196a4e9ba7bbdc1286a6
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-23 19:48:40 +0100

    Update many files

--- client/src/pages/patientReg.js
+++ client/src/pages/patientReg.js
@@ -1,30 +1,14 @@
 // SignupForm.js
-<<<<<<< HEAD
-import React, { useState } from "react";
-import "../styles/AuthForm.css";
-import logo from "../icons/logo2.png";
-import PatientLogin from "./patientLogin";
-import Confirmation from "./confimationLink";
-import google from '../icons/google.png'
-
-=======
 import React, { useState } from 'react';
 import '../styles/AuthForm.css';
 import logo from '../icons/logo.svg';
 import PatientLogin from './patientLogin';
-import Confirmation from './confimationLink';
 import Dashboard from './dashboard';
->>>>>>> e2ffda6 (Update many files)
 
 const PatientSignUp = (props) => {
 
   const { api, api2 } = props;
-<<<<<<< HEAD
-  console.log(api, api2);
-=======
  
-
->>>>>>> e2ffda6 (Update many files)
   const [showLogin, setShowLogin] = useState(false);
   const [dashboard, setDashboard] = useState(false);
   const [name, setName] = useState('');
@@ -34,19 +18,13 @@ const PatientSignUp = (props) => {
     setShowLogin(true);
   }
 
-<<<<<<< HEAD
-  function handleConfirmation() {
-    setConfirmation(true);
-  }
-=======
 
->>>>>>> e2ffda6 (Update many files)
 
   const [signupData, setSignupData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
   });
 
   const handleChange = (e) => {
@@ -95,111 +73,6 @@ const PatientSignUp = (props) => {
       // Optionally show an error message to the user
     }
   };
-<<<<<<< HEAD
-
-  return (
-    <>
-      {!showLogin && !confirmation && (
-        <form
-          className="auth-form"
-          onSubmit={handleSubmit}
-          method="post"
-          
-      
-        >
-          <div className="logo">
-            <img src={logo} alt="logo" width={40} height={40} />
-            <h3 style={{ color: "#0E9061" }}>
-              Med<span style={{ color: "#000000" }}>Ease</span>
-            </h3>
-          </div>
-          <div className="form-header">
-            <h2>Welcome to MedEase</h2>
-            <p>Create your Account</p>
-          </div>
-
-          <label>
-            First Name
-            <input
-              type="text"
-              name="firstName"
-              value={signupData.firstName}
-              placeholder="First Name"
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <label>
-            Last Name
-            <input
-              type="text"
-              name="lastName"
-              value={signupData.lastName}
-              onChange={handleChange}
-              placeholder="Last Name"
-              required
-            />
-          </label>
-          <label>
-            Email
-            <input
-              type="email"
-              name="email"
-              value={signupData.email}
-              onChange={handleChange}
-              placeholder="Email Address"
-              required
-            />
-          </label>
-          <label>
-            Password
-            <div className="password-input">
-              <input
-                type="password"
-                name="password"
-                value={signupData.password}
-                onChange={handleChange}
-                placeholder="8 characters"
-                required
-              />
-            </div>
-          </label>
-          <div className="checkbox-container">
-            <input type="checkbox" />
-            <span>Remember Password</span>
-          </div>
-
-          <button type="submit">
-            Create Account
-          </button>
-          
-          <div className="or-divider">
-            <hr />
-          <span className="or-text">or</span>
-            <hr />
-          </div>
-          <div className="google-signup">
-            <img src={google} width={20} height={20} alt="google" />
-            <a href="/">Sign up With Google</a>
-          </div>
-          <div className="login">
-            <p>
-              By clicking on “create account” you agree to MedEase <br/>
-              <a href="/">terms and conditions</a>
-            </p>
-            <p>
-              Already have an account?{" "}
-              <a href="#" onClick={handleLogin}>
-                Sign In
-              </a>
-            </p>
-          </div>
-         
-        </form>
-      )}
-
-      {showLogin && !confirmation && <PatientLogin  api2={api2} />}
-=======
   
    
 
@@ -289,11 +162,8 @@ const PatientSignUp = (props) => {
     )}
 
     {showLogin && !dashboard && <PatientLogin  api2={api2} />}
->>>>>>> e2ffda6 (Update many files)
 
     {!showLogin && dashboard && <Dashboard  name={name} />}
-
-    
 
     </>
   );

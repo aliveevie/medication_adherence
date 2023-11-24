3e1c6cd31d0ed5b892c35a1dca0e80c6eb4c4b59
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-24 16:31:54 +0100

    Update appointment

--- client/src/pages/dashboard/Medication.js
+++ client/src/pages/dashboard/Medication.js
@@ -1,19 +1,37 @@
-export default function Medication(props){
-    const {name, dName, date, time } = props;
-        return (
-            <>
-            <div>
-                <div className='dash-body'>
-            <div className='dash-text'>
-                <h2>Hello {name}! Welcome</h2>
-                <p>Your next appointment with Dr. 
-                    {dName} is scheduled for {date}, {time}</p>
-            </div>
-          </div>
-          <div className="active-med" >
-            <p>Active Medication</p>
+import React from 'react';
+
+export default function Medication(props) {
+  let { name, dName, date, time } = props;
+
+  const formatDate = (inputDate) => {
+    const options = { day: 'numeric', month: 'short' };
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(new Date(inputDate));
+    return formattedDate;
+  };
+
+  const formatTime = (inputTime) => {
+    const [hours, minutes] = inputTime.split(':');
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+    const formattedTime = new Intl.DateTimeFormat('en-US', options).format(new Date(0, 0, 0, hours, minutes));
+    return formattedTime;
+  };
+  
+
+  date = formatDate(date);
+
+  return (
+    <>
+      <div>
+        <div className='dash-body'>
+          <div className='dash-text'>
+            <h2>Hello {name}! Welcome</h2>
+            <p>Your next appointment with Dr. {dName} is scheduled for {date}, {formatTime(time)}</p>
           </div>
         </div>
-            </>
-        )
+        <div className="active-med">
+          <p>Active Medication</p>
+        </div>
+      </div>
+    </>
+  );
 }

0fe74bd594fbfad8e06a0f278c4f76abec7adc10
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-25 15:32:06 +0100

    Update many files

--- client/src/pages/dashboard/eachMed.js
+++ client/src/pages/dashboard/eachMed.js
@@ -2,6 +2,7 @@ import React from 'react';
 import back from '../../icons/back.svg';
 import '../../styles/activemed.css';
 import formatTime from '../../functions/formatTime';
+import icons from '../../icons/icons.svg';
 
 export default function EachMed({ handleCurrent, medication }) {
  
@@ -12,8 +13,18 @@ export default function EachMed({ handleCurrent, medication }) {
       <img src={back} alt='Medication details' onClick={handleCurrent} />
       <div className='medication-item'>
         <h2>{medication.medicationname} - {medication.dose}</h2>
-        <p>Today {formatTime(medication.time)}</p>
-        {/* Add more details if needed */}
+        <div className='medication-stop'>
+            <img src={icons} alt='Stop Icon' />
+             <p>Today {formatTime(medication.time)}</p>
+        </div>
+      </div>
+      <p>Day 1</p>
+      <div className='check-in' >
+        
+        <div className='days' >
+        <p>Pending</p>
+        <p>Check In</p>
+        </div>
       </div>
     </div>
   );
